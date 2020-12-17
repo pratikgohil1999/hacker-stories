d93c36b67a7da5ab48c6c8c5e3c5a67200f56489
pratikgohil1999 <pratikgohil008@gmail.com.com>
2020-12-18 01:07:48 +0530

    remove stories feature added

--- src/App.js
+++ src/App.js
@@ -1,29 +1,50 @@
 import React from 'react';
 
-const App = () => {
-  const stories = [
-    {
-      title: 'React',
-      url: 'https://reactjs.org/',
-      author: 'Jordon Walke',
-      num_comments: 3,
-      points: 4,
-      objectID: 0,
-    },
-    {
-      title: 'Redux',
-      url: 'https://redux.js.org/',
-      author: 'Dan Abramov, Andrew Clark',
-      num_comments: 2,
-      points: 5,
-      objectID: 1,
-    },
-  ];
+const initialStories = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordon Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 
-  const [searchTerm, serSearchTerm] = React.useState('React');
+const useSemiPersistentState = (key, initialState) => {
+  const [value, setValue] = React.useState(
+    localStorage.getItem(key) || initialState
+  );
+
+  React.useEffect(() => {
+    localStorage.setItem(key, value);
+  }, [value, key]);
+
+  return [value, setValue];
+};
+
+const App = () => {
+  const [searchTerm, setSearchTerm] = useSemiPersistentState('search', 'React');
+
+  const [stories, setStories] = React.useState(initialStories);
+
+  const handleRemoveStory = (item) => {
+    const newStories = stories.filter(
+      (story) => item.objectID !== story.objectID
+    );
+    setStories(newStories);
+  };
 
   const handleSearch = (event) => {
-    serSearchTerm(event.target.value);
+    setSearchTerm(event.target.value);
   };
 
   const searchedStories = stories.filter(story =>
@@ -33,38 +54,61 @@ const App = () => {
   return (
     <div>
       <h1>My Hacker Stories</h1>
-      <Serach search={searchTerm} onSearch={handleSearch} />
+
+      <InputWithLabel
+        id='search'
+        value={searchTerm}
+        isFocused
+        onInputChange={handleSearch}
+      >
+        <strong>Search : </strong>
+      </InputWithLabel>
+
       <hr />
-      <List list={searchedStories} />
+
+      <List list={searchedStories} onRemoveItem={handleRemoveStory} />
     </div>
   );
 };
 
-const Serach = ({ search, onSearch }) => (
-  <div>
-    <label htmlFor="search">Search :</label>
+const InputWithLabel = ({ id, value, type = 'text', onInputChange, isFocused, children, }) => (
+  <>
+    <label htmlFor={id}>{children}</label>
+    &nbsp;
     <input
-      id="search"
-      type="text"
-      value={search}
-      onChange={onSearch}
+      id={id}
+      type={type}
+      value={value}
+      autoFocus={isFocused}
+      onChange={onInputChange}
     />
-  </div>
+  </>
 );
 
-const List = ({ list }) =>
-  list.map(({ objectID, ...item }) => <Item key={objectID}{...item} />);
+const List = ({ list, onRemoveItem }) =>
+  list.map(item => (
+    <Item
+      key={item.objectID}
+      item={item}
+      onRemoveItem={onRemoveItem}
+    />
+  ));
 
-const Item = ({ title, url, author, num_comments, points }) => (
-  <div>
-    <span>
-      <a href={url}>{title}</a>
-    </span>
-    <span> {author}</span>
-    <span> {num_comments}</span>
-    <span> {points}</span>
-  </div>
-);
+const Item = ({ item, onRemoveItem }) => (
+    <div>
+      <span>
+        <a href={item.url}>{item.title}</a>
+      </span>
+      <span>{item.author}</span>
+      <span>{item.num_comments}</span>
+      <span>{item.points}</span>
+      <span>
+        <button type='button' onClick={() => onRemoveItem.bind(null, item)}>
+          Dismiss
+        </button>
+      </span>
+    </div>
+  );
 
 export default App;
 
